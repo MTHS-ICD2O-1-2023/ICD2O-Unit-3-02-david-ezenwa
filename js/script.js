7176c58e4a5480a0367d1178443dcb5f6953de0c
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-16 18:54:37 +0000

    made a web page that calculates the volume of a righht rectangular pyramid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: David.E
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const baseL = parseFloat(document.getElementById('base-l').value);
+  const baseW = parseFloat(document.getElementById('base-w').value);
+  const heightOfPyramid = parseFloat(document.getElementById('height-of-pyramid').value);
+
+  // process
+  const areaOfPyramid = (baseL * baseW * heightOfPyramid) / 3;
+
+  // output
+  document.getElementById("area").innerHTML = 'Area is: ' + areaOfPyramid.toFixed(2) + ' mm³';
+}
